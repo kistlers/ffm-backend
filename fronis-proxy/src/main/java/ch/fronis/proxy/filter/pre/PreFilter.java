60caebc6e1d1d876edd01dc4a4ec1c6cb3ff6ff8
Severin Kistler <kistler@ubique.ch>
2020-07-03 22:10:40 +0200

    porxy filters for logs

---
+++ fronis-proxy/src/main/java/ch/fronis/proxy/filter/pre/PreFilter.java
@@ -0,0 +1,39 @@
+package ch.fronis.proxy.filter.pre;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class PreFilter extends ZuulFilter {
+
+    private static final Logger log = LoggerFactory.getLogger(PreFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return false;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+
+        log.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL()));
+
+        return null;
+    }
+}
