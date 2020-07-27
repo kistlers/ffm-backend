7eaf596f78a3f6e408761eb732668cf77e8b56be
Severin Kistler <kistler@ubique.ch>
2020-07-27 12:47:21 +0200

    0.0.1-SNAPSHOT replace by 0.0.1

--- docker/copy_to_pi.sh
+++ docker/copy_to_pi.sh
@@ -3,5 +3,5 @@ if [ $# -eq 0 ]; then
     exit
 fi
 
-docker save fronis-$1:0.0.1-SNAPSHOT | bzip2 | pv | ssh rpi 'bunzip2 | docker load'
+docker save fronis-$1:0.0.1 | bzip2 | pv | ssh rpi 'bunzip2 | docker load'
 
