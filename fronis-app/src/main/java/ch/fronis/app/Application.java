35630c4e01381ab87af8cca8d46ed9e99187be48
Severin Kistler <kistler@bique.ch>
2020-08-16 23:40:52 +0200

    global cors

--- fronis-app/src/main/java/ch/fronis/app/Application.java
+++ fronis-app/src/main/java/ch/fronis/app/Application.java
@@ -2,8 +2,11 @@ package ch.fronis.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
@@ -12,6 +15,16 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost", "https://r22.internet-box.ch");
+            }
+        };
     }
 
 }
