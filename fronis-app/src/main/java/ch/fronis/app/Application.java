fa1e52d270490d714fbc4b420a586b08825b7768
Severin Kistler <kistlerseverin@gmail.com>
2020-08-17 00:10:56 +0200

    fix cors

--- fronis-app/src/main/java/ch/fronis/app/Application.java
+++ fronis-app/src/main/java/ch/fronis/app/Application.java
@@ -16,15 +16,4 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost", "https://r22.internet-box.ch");
-            }
-        };
-    }
-
 }
