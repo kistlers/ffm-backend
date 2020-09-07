adaad8bf6e6e46eb1e148b5f2ea9c6fbc05b03d4
Severin Kistler <kistler@bique.ch>
2020-09-07 11:45:38 +0200

    fix db migration

--- fronis-data/src/main/resources/db/migration/mysql/V0_1__init.sql
+++ fronis-data/src/main/resources/db/migration/mysql/V0_1__init.sql
@@ -10,14 +10,3 @@ CREATE TABLE t_player
     image         MEDIUMTEXT,
     PRIMARY KEY (pk_player_id)
 );
-
-replace into t_player(first_name, last_name, short_name, player_number, position, year_of_birth, image)
-values ('Severin', 'Kistler', 'Sevi', 29, 'FIELD', 1997, null);
-replace into t_player(first_name, last_name, short_name, player_number, position, year_of_birth, image)
-values ('Nicolas', 'Kistler', 'Nici', 13, 'FIELD', 1992, null);
-replace into t_player(first_name, last_name, short_name, player_number, position, year_of_birth, image)
-values ('Thomas', 'Stüssi', 'Stü', 7, 'FIELD', 1988, null);
-replace into t_player(first_name, last_name, short_name, player_number, position, year_of_birth, image)
-values ('Remo', 'Eberle', 'Showgoalie', 1, 'GOAL', 1985, null);
-replace into t_player(first_name, last_name, short_name, player_number, position, year_of_birth, image)
-values ('Jack', 'Hösli', 'Ebi', null, 'STAFF', 1960, null);
