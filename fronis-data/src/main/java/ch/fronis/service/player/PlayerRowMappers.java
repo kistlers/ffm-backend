093a4625d8e5c59869618a64109361365193e28d
Severin Kistler <kistler@bique.ch>
2020-09-26 22:39:56 +0200

    fix fetching integer null as 0

--- fronis-data/src/main/java/ch/fronis/service/player/PlayerRowMappers.java
+++ fronis-data/src/main/java/ch/fronis/service/player/PlayerRowMappers.java
@@ -16,9 +16,9 @@ public class PlayerRowMappers {
             int id = resultSet.getInt("pk_player_id");
             String firstName = resultSet.getString("first_name");
             String lastName = resultSet.getString("last_name");
-            Integer playerNumber = resultSet.getInt("player_number");
+            Integer playerNumber = resultSet.getObject("player_number", Integer.class);
             PlayerPosition position = PlayerPosition.fromString(resultSet.getString("position"));
-            Integer yearOfBirth = resultSet.getInt("year_of_birth");
+            Integer yearOfBirth = resultSet.getObject("year_of_birth", Integer.class);
             String image = resultSet.getString("image");
             return new Player(id, firstName, lastName, playerNumber, position, yearOfBirth, image);
         }
