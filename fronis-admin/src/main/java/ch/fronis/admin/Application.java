962b298149e286e28b3dc57a8b4c773beeae7878
Severin Kistler <kistler@ubique.ch>
2020-10-11 14:42:07 +0200

    fix basepackage

--- fronis-admin/src/main/java/ch/fronis/admin/Application.java
+++ fronis-admin/src/main/java/ch/fronis/admin/Application.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @EnableWebMvc
 @SpringBootApplication(
-        scanBasePackages = {"ch.fronis.admin.config", "ch.fronis.admin.hibernate"})
+        scanBasePackages = {"ch.fronis.admin.config", "ch.fronis.admin.controller"})
 @EntityScan("ch.fronis.model.entity")
 @EnableJpaRepositories("ch.fronis.data.repository")
 public class Application {
