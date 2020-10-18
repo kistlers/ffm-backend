72e5b0ea42c5a217587429b06d7aa3791c2a2045
Severin Kistler <kistler@ubique.ch>
2020-10-18 21:58:38 +0200

    sponsors in app

--- fronis-data/src/main/java/ch/fronis/data/repository/SponsorRepository.java
+++ fronis-data/src/main/java/ch/fronis/data/repository/SponsorRepository.java
@@ -1,12 +1,16 @@
 package ch.fronis.data.repository;
 
 import ch.fronis.model.entity.Sponsor;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface SponsorRepository
-        extends JpaRepository<Sponsor, Integer>, JpaSpecificationExecutor<Sponsor   > {
+        extends JpaRepository<Sponsor, Integer>, JpaSpecificationExecutor<Sponsor> {
+
+    List<Sponsor> findAllByOrderByOrderingAsc();
+
     Page<Sponsor> findAllByOrderByOrderingAsc(Pageable pageable);
 }
