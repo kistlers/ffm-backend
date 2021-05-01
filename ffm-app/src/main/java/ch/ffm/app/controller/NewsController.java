6b908fa6163d6f113b05ce1a474a0adec8d8e241
Severin Kistler <kistler@ubique.ch>
2021-05-01 17:49:16 +0200

    java 11 is enough

--- ffm-app/src/main/java/ch/ffm/app/controller/NewsController.java
+++ ffm-app/src/main/java/ch/ffm/app/controller/NewsController.java
@@ -3,9 +3,11 @@ package ch.ffm.app.controller;
 import ch.ffm.app.config.HeaderHelper;
 import ch.ffm.data.repository.NewsRepository;
 import ch.ffm.model.entity.News;
+import java.text.MessageFormat;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,12 +21,24 @@ public class NewsController {
         this.newsRepository = newsRepository;
     }
 
-    /**
-     * @return news ordered by publishTimestamp
-     */
+    /** @return news ordered by publishTimestamp */
     @GetMapping({"", "/"})
     public ResponseEntity<List<News>> news() {
         var news = newsRepository.findAllByOrderByCreatedAtDesc();
+        return HeaderHelper.createOKResponseEntityDefaultCacheControl(news);
+    }
+
+    @GetMapping("/{newsId}")
+    public ResponseEntity<News> oneNews(final @PathVariable int newsId) {
+        var news =
+                newsRepository
+                        .findById(newsId)
+                        .orElseThrow(
+                                () ->
+                                        new RuntimeException(
+                                                MessageFormat.format(
+                                                        "Cannot handle news GET request. News with id={0} not found",
+                                                        newsId)));
         return HeaderHelper.createOKResponseEntityDefaultCacheControl(news);
     }
 }
